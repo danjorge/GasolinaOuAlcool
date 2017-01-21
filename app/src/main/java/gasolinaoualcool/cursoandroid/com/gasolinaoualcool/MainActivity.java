13bce49559c0e1ac6fd9e50fd46f5130b22e27ef
Daniel <danjorge@gmail.com>
2017-01-21 19:56:37 -0300

    first commit

---
+++ app/src/main/java/gasolinaoualcool/cursoandroid/com/gasolinaoualcool/MainActivity.java
@@ -0,0 +1,87 @@
+package gasolinaoualcool.cursoandroid.com.gasolinaoualcool;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText precoAlcool;
+    private EditText precoGasolina;
+    private Button botaoVerificar;
+    private TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        precoAlcool = (EditText) findViewById(R.id.precoAlcoolId);
+        precoGasolina = (EditText) findViewById(R.id.precoGasolinaId);
+        botaoVerificar = (Button) findViewById(R.id.botaoVerificarId);
+        textoResultado = (TextView) findViewById(R.id.textoResultadoId);
+
+        botaoVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textoPrecoAlcool = precoAlcool.getText().toString();
+                String textoPrecoGasolina = precoGasolina.getText().toString();
+
+                Double valorAlcool = Double.parseDouble(textoPrecoAlcool);
+                Double valorGasolina = Double.parseDouble(textoPrecoGasolina);
+
+                double resultado = valorAlcool / valorGasolina;
+
+                if(resultado >= 0.7){
+                    textoResultado.setText("É melhor utilizar Gasolina");
+                } else {
+                    textoResultado.setText("É melhor utilizar Álcool");
+                }
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
